a30f6bb38631422340df614889f3527b1c598e26
iCodeWoods <963239327@qq.com>
2016-05-12 17:20:39 +0800

    大幅度优化代码

--- UIBezierPath实现画图板/UIBezierPath实现画图板/ViewController.swift
+++ UIBezierPath实现画图板/UIBezierPath实现画图板/ViewController.swift
@@ -22,47 +22,47 @@ class ViewController: UIViewController {
     // MARK:- life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        drawingBoard.frame = CGRectMake(0, 0, ScreenWidth, ScreenHeight*2/3)
+        drawingBoard.frame = CGRect(x: 0, y: 0, width: ScreenWidth, height: ScreenHeight*2/3)
         view.addSubview(drawingBoard)
         
-        let line = UIView.init(frame: CGRectMake(0, CGRectGetMaxY(drawingBoard.frame), ScreenWidth, 0.5))
+        let line = UIView(frame: CGRect(x: 0, y: drawingBoard.frame.maxY, width: ScreenWidth, height: 0.5))
         line.backgroundColor = UIColor.grayColor()
         view.addSubview(line)
         
-        let sliderLabel = UILabel.init(frame: CGRectMake(30, CGRectGetMaxY(drawingBoard.frame)+20, 100, 40))
+        let sliderLabel = UILabel(frame: CGRect(x: 30, y: drawingBoard.frame.maxY+20, width: 100, height: 40))
         sliderLabel.text = "线条宽度"
         view.addSubview(sliderLabel)
         
-        mySlider.frame = CGRectMake(ScreenWidth-sliderLabel.frame.origin.x-100, sliderLabel.frame.origin.y, 100, sliderLabel.frame.size.height)
-        mySlider.addTarget(self, action: Selector("onClickSlider:"), forControlEvents: UIControlEvents.ValueChanged)
+        mySlider.frame = CGRect(x: ScreenWidth-sliderLabel.frame.origin.x-100, y: sliderLabel.frame.origin.y, width: 100, height: sliderLabel.frame.height)
+        mySlider.addTarget(self, action: Selector("onClickSlider:"), forControlEvents: .ValueChanged)
         mySlider.maximumValue = 3
         view.addSubview(mySlider)
         
-        let segentmentLabel = UILabel.init(frame: CGRectMake(sliderLabel.frame.origin.x, CGRectGetMaxY(sliderLabel.frame)+20, sliderLabel.frame.width, sliderLabel.frame.height))
+        let segentmentLabel = UILabel(frame: CGRect(x: sliderLabel.frame.origin.x, y: sliderLabel.frame.maxY+20, width: sliderLabel.frame.width, height: sliderLabel.frame.height))
         segentmentLabel.text = "线条颜色"
         view.addSubview(segentmentLabel)
         
-        mySegment.frame = CGRectMake(mySlider.frame.origin.x, segentmentLabel.frame.origin.y, mySlider.frame.width, mySlider.frame.height)
-        mySegment.addTarget(self, action: Selector("onClickSegment:"), forControlEvents: UIControlEvents.ValueChanged)
+        mySegment.frame = CGRect(x: mySlider.frame.origin.x, y: segentmentLabel.frame.origin.y, width: mySlider.frame.width, height: mySlider.frame.height)
+        mySegment.addTarget(self, action: Selector("onClickSegment:"), forControlEvents: .ValueChanged)
         view.addSubview(mySegment)
         
-        backBtn.frame = CGRectMake(segentmentLabel.frame.origin.x, CGRectGetMaxY(segentmentLabel.frame)+20, 80, 40)
-        backBtn.addTarget(self, action: Selector("onClickBack:"), forControlEvents: UIControlEvents.TouchUpInside)
-        backBtn.setTitle("后退", forState: UIControlState.Normal)
-        backBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        backBtn.frame = CGRect(x: segentmentLabel.frame.origin.x, y: segentmentLabel.frame.maxY+20, width: 80, height: 40)
+        backBtn.addTarget(self, action: Selector("onClickBack:"), forControlEvents: .TouchUpInside)
+        backBtn.setTitle("后退", forState: .Normal)
+        backBtn.setTitleColor(UIColor.blackColor(), forState: .Normal)
         view.addSubview(backBtn)
         
-        saveBtn.frame = CGRectMake(0, 0, backBtn.frame.width, backBtn.frame.height)
-        saveBtn.center = CGPointMake(ScreenWidth/2, backBtn.frame.origin.y+saveBtn.frame.height/2)
-        saveBtn.addTarget(self, action: Selector("onClickSave:"), forControlEvents: UIControlEvents.TouchUpInside)
+        saveBtn.frame = CGRect(x: 0, y: 0, width: backBtn.frame.width, height: backBtn.frame.height)
+        saveBtn.center = CGPoint(x: ScreenWidth/2, y: backBtn.frame.origin.y+saveBtn.frame.height/2)
+        saveBtn.addTarget(self, action: Selector("onClickSave:"), forControlEvents: .TouchUpInside)
         saveBtn.setTitle("保存", forState: UIControlState.Normal)
-        saveBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        saveBtn.setTitleColor(UIColor.blackColor(), forState: .Normal)
         view.addSubview(saveBtn)
         
-        forwardBtn.frame = CGRectMake(ScreenWidth-backBtn.frame.width-backBtn.frame.origin.x, backBtn.frame.origin.y, backBtn.frame.width, backBtn.frame.height)
-        forwardBtn.addTarget(self, action: Selector("onClickForward:"), forControlEvents: UIControlEvents.TouchUpInside) // TODO: #selector(onClickForward(_:)) ???
-        forwardBtn.setTitle("前进", forState: UIControlState.Normal)
-        forwardBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        forwardBtn.frame = CGRect(x: ScreenWidth-backBtn.frame.width-backBtn.frame.origin.x, y: backBtn.frame.origin.y, width: backBtn.frame.width, height: backBtn.frame.height)
+        forwardBtn.addTarget(self, action: Selector("onClickForward:"), forControlEvents: .TouchUpInside)
+        forwardBtn.setTitle("前进", forState: .Normal)
+        forwardBtn.setTitleColor(UIColor.blackColor(), forState: .Normal)
         view.addSubview(forwardBtn)
     }
     
@@ -99,7 +99,7 @@ class ViewController: UIViewController {
         UIGraphicsEndImageContext() // 结束截取
         UIImageWriteToSavedPhotosAlbum(img, nil, nil, nil) // 把截取到的图像保存到相册中
         // 最后提示用户保存成功即可
-        let alert = UIAlertView.init(title: "存储照片成功", message: "您已将照片存储于图片库中，打开照片程序即可查看。", delegate: self, cancelButtonTitle: "OK")
+        let alert = UIAlertView(title: "存储照片成功", message: "您已将照片存储于图片库中，打开照片程序即可查看。", delegate: self, cancelButtonTitle: "OK")
         alert.show()
     }
 }
